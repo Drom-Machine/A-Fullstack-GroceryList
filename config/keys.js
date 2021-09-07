ece6e89145af8a22105c1931b86ed53b8e4e44ab
Drom-Machine <propgmachine@gmail.com>
2021-09-06 23:54:41 -0400

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.export = {
+        mongoURI: 'mongosh "mongodb+srv://cluster0.ly2mb.mongodb.net/myFirstDatabase" --username Steve'
+}
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Grocery from './components/Groceries/Grocery'
+import Header from "./components/Header/Header"
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Grocery/>
+      </div>
+    )
+  }
+}
+
+export default App;
